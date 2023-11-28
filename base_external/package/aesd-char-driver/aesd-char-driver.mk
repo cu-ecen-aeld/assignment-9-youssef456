8aafef45fff40252acd6979c25e976d8444cef76
youssef456 <yousefnagy2@gmail.com>
2023-11-28 16:07:21 +0200

    assignment-9

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -3,8 +3,11 @@
 # aesd-char-driver
 #
 ################################################################################
-
-AESD_CHAR_DRIVER_VERSION = 'e5f71bd'
+#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
+#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
+#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
+#AESD_CHAR_DRIVER_VERSION = 'e5f71bd'
+AESD_CHAR_DRIVER_VERSION = '24c4b05'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,12 +21,6 @@ define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
-
-	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
-	
-
 
 endef
 
